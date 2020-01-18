429c4daa37c556ab72156710537a11bce823dc25
EdwardStables <edward.stables1198@gmail.com>
2020-01-18 19:26:29 +0000

    messages reflected in game logic

--- backend/game_logic.py
+++ backend/game_logic.py
@@ -7,13 +7,60 @@ import asyncio
 import server
 from time import sleep
 
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 class Game:
 
     def __init__(self, **kwargs):
         self.board = self.generate_map()
+        self.actions = {
+                "move_left"  : self.move_player_left,
+                "move_right" : self.move_player_right,
+                "move_up"    : self.move_player_up,
+                "move_down"  : self.move_player_down,
+                "message"    : self.handle_message,
+                "push_left"  : self.push_player_left,
+                "push_right" : self.push_player_right,
+                "push_up"    : self.push_player_up,
+                "push_down"  : self.push_player_down,
+                }
+        self.players = {}
+
+    def add_player(self, id_val):
+        self.players[id_val] = Player(1, 2)
 
     def generate_map(self):
         return np.loadtxt("grid_example.txt")
+
+    def move_player_left(self, player):
+        print("Moving left")
+
+    def move_player_right(self, player):
+        print("Moving right")
+
+    def move_player_up(self, player):
+        print("Moving up")
+
+    def move_player_down(self, player):
+        print("Moving down")
+
+    def handle_message(self, player, message):
+        print(f"Message: {message}")
+
+    def push_player_left(self, player):
+        print("Pushing left")
+
+    def push_player_right(self, player):
+        print("Pushing right")
+
+    def push_player_up(self, player):
+        print("Pushing up")
+
+    def push_player_down(self, player):
+        print("Pushing down")
 
 def load_config(path):
     with open(path) as f:
@@ -23,7 +70,6 @@ def load_config(path):
 if __name__ == "__main__":
     config = load_config("./config.json") 
     game = Game(config=config)
-    print(game.board)
-    #server.run_server("127.0.0.1", 5678, game)
+    server.run_server("127.0.0.1", 5678, game)
     
 
--- backend/server.py
+++ backend/server.py
@@ -4,16 +4,18 @@ import random, string, json
 
 connections = {}
 
-async def main(websocket, path):
+async def main(game, websocket, path):
     # a unique id for the connection
     ws_id = id(websocket)
     if ws_id not in connections:
         uID = str(uuid.uuid4())
-        connections[id(websocket)] = {
+        game.add_player(ws_id) 
+        connections[ws_id] = {
             'sock' : websocket,
             'uID' : uID,
+            'action' : "",
+            'message' : "",
         }
-    #    await websocket.send(game
 
 
     print(f"Responding to {ws_id}")
@@ -25,7 +27,8 @@ async def main(websocket, path):
 
     try:
         async for message in websocket:
-            print(message)
+            print(f"New action: {message}")
+            connections[ws_id]["action"] = message
 
             #data = json.loads(message)
 
@@ -44,12 +47,25 @@ async def main(websocket, path):
 #            await vals["sock"].send(vals["uID"])
 #        await asyncio.sleep(3)
 
+async def game_tick(game):
+    while True:
+        for player_id, vals in connections.items():
+            act = vals["action"]
+            if act == "message": 
+                game.message(player_id, vals["message"])
+            elif act in game.actions:
+                game.actions[act](player_id)
+            vals["action"] = ""
+        await asyncio.sleep(0.05)
+
+
 def run_server(serverIP, port, game):
     print('Starting socket server....')
     print(f'Running on http://{serverIP}:{port}')
-    start_server = websockets.serve(main, serverIP, port)
+    start_server = websockets.serve(lambda x, y:main(game, x, y), serverIP, port)
     
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_server)
-    loop.create_task(status())
+    #loop.create_task(status())
+    loop.create_task(game_tick(game))
     asyncio.get_event_loop().run_forever()
